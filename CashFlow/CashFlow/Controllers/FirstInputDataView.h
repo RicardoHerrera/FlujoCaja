ba18819d5290319b46d1e5c97564ceeb2748269a
RicardoHerrera <aherreric@gmail.com>
2013-06-23 11:06:47 -0500

    input Data

---
+++ CashFlow/CashFlow/Controllers/FirstInputDataView.h
@@ -0,0 +1,13 @@
+//
+//  FirstInputDataView.h
+//  CashFlow
+//
+//  Created by Ricardo Herrera Valle on 23/06/13.
+//  Copyright (c) 2013 UPCFinance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstInputDataView : UIViewController
+
+@end
---
+++ CashFlow/CashFlow/Controllers/InputDataView.m
@@ -0,0 +1,38 @@
+//
+//  InputDataView.m
+//  CashFlow
+//
+//  Created by Ricardo Herrera Valle on 23/06/13.
+//  Copyright (c) 2013 UPCFinance. All rights reserved.
+//
+
+#import "InputDataView.h"
+
+@interface InputDataView ()
+
+@end
+
+@implementation InputDataView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
