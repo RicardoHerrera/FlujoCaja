d02c06974c2ad156eaf528085e59af0611b2f167
RicardoHerrera <aherreric@gmail.com>
2013-06-22 20:48:44 -0500

    empty cells for uicollectionview :D

--- CashFlow/CashFlow/Controllers/MainMenuViewController.h
+++ CashFlow/CashFlow/Controllers/MainMenuViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MainMenuViewController : UIViewController <UICollectionViewDataSource, UICollectionViewDelegate>{
+@interface MainMenuViewController : UIViewController <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>{
     
     NSArray *arraryData;
     
--- CashFlow/CashFlow/Controllers/MainMenuViewController.m
+++ CashFlow/CashFlow/Controllers/MainMenuViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MainMenuViewController.h"
+#import "CashFlowService.h"
 
 @interface MainMenuViewController ()
 
@@ -27,6 +28,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    CashFlowService *cashService = [[CashFlowService alloc] init];
+    
+    arraryData = [[NSArray alloc] init];
+    arraryData = [cashService getCashFlows];
+    
+    
+    NSLog(@"%@", [cashService getCashFlows]);
 }
 
 #pragma mark CollectionView DataSource
@@ -35,11 +43,24 @@
     return [arraryData count];
 }
 
+- (NSInteger)numberOfSectionsInCollectionView: (UICollectionView *)collectionView {
+    return 1;
+}
+
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     
-    UICollectionViewCell *cell;
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CashFlow" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor whiteColor];
     
     return cell;
+}
+#pragma mark CollectionView Delegate
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    // TODO: Select Item
+}
+- (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath {
+    // TODO: Deselect item
 }
 
 - (void)didReceiveMemoryWarning
