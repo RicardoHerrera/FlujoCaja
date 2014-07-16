c626bcbd9d24dbac5583e9a67866e4fc39395288
Ricardo <ricardoherrera@pe-jballon.avanticape.avantica.avanticatec.net>
2014-07-16 15:51:36 -0500

    Prueba

--- CashFlow/CashFlow/Factories/AlertViewsFactory.h
+++ CashFlow/CashFlow/Factories/AlertViewsFactory.h
@@ -3,7 +3,7 @@
 //  CashFlow
 //
 //  Created by Alex Gutierrez on 5/25/13.
-//  Copyright (c) 2013 UPCFinance. All rights reserved.
+//  Copyright (c) 2013 UPCFinance. All rights reserved..
 //
 
 #import <Foundation/Foundation.h>
